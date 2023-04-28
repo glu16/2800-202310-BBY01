a3cda29c77a3020f1f3ea7e1cf160c350281d46b
Gin Lu <ginlu7324@gmail.com>
2023-04-28 16:33:20 -0700

    Added in SignUp.jsx and Login.jsx

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import NavbarAfterLogin from './components/NavbarAfterLogin';
-import Home from './components/Home';
-import Fitness from './components/Fitness';
-import Diet from './components/Diet';
-import Leaderboard from './components/Leaderboard';
-import Profile from './components/Profile';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import NavbarAfterLogin from "./components/NavbarAfterLogin";
+import Home from "./components/Home";
+import Fitness from "./components/Fitness";
+import Diet from "./components/Diet";
+import Leaderboard from "./components/Leaderboard";
+import Profile from "./components/Profile";
 
-import './css/App.css';
-import './css/main.css';
-import './css/navfooter.css';
+import "./css/App.css";
+import "./css/main.css";
+import "./css/navfooter.css";
 
 function App() {
   return (
     <Router>
       {/* <div> */}
-        <NavbarAfterLogin />
-        <div>
+      <NavbarAfterLogin />
+      <div>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/fitness" element={<Fitness />} />
@@ -24,7 +24,7 @@ function App() {
           <Route path="/leaderboard" element={<Leaderboard />} />
           <Route path="/profile" element={<Profile />} />
         </Routes>
-        </div>
+      </div>
       {/* </div> */}
     </Router>
   );
---
+++ src/components/Login.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+
+function Login() {
+  return (
+    <div className="fullHeight" id="loginCon">
+      <div id="formContainer">
+        <h1>Login</h1>
+        <form id="login" action="/loggingin">
+          <label htmlFor="email-input"></label>
+          <input
+            type="email"
+            id="email-input"
+            className="user-input"
+            placeholder="Email"
+            name="email"
+            required
+            size="30"
+          />
+
+          <label htmlFor="password-input"></label>
+          <input
+            type="password"
+            id="password-input"
+            className="user-input"
+            name="password"
+            placeholder="Password"
+            required
+          />
+
+          <label htmlFor="submit-btn"></label>
+          <input type="submit" id="login-btn" value="Login" />
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
