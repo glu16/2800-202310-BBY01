0977b06f8eb557b9825c423368ff1c80c46f23af
Gin Lu <glu23@my.bcit.ca>
2023-05-04 13:28:27 -0700

    Added Index.jsx to App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import NavbarAfterLogin from "./components/NavbarAfterLogin";
+import Index from "./components/Index";
 import SignUp from "./components/SignUp";
 import Login from "./components/Login";
 
 import Home from "./components/Home";
-import SignupDetails from "./components/SignupDetails"
+import SignupDetails from "./components/SignupDetails";
 import Coach from "./components/Coach";
 import Diet from "./components/Diet";
 import Fitness from "./components/Fitness";
@@ -18,19 +19,20 @@ function App() {
   return (
     <Router>
       <NavbarAfterLogin />
-        <Routes>
-          <Route path="/signup" element={<SignUp />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/signupdetails" element={<SignupDetails />} />
-          <Route path="/home" element={<Home />} />
-          <Route path="/coach" element={<Coach />} />
-          <Route path="/diet" element={<Diet />} />
-          <Route path="/fitness" element={<Fitness />} />
-          <Route path="/leaderboard" element={<Leaderboard />} />
-          <Route path="/calendar" element={<Calendar />} />
-          <Route path="/profile" element={<Profile />} />
-          <Route path="/settings" element={<Settings />} />
-        </Routes>
+      <Routes>
+        <Route path="/index" element={<Index />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signupdetails" element={<SignupDetails />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/coach" element={<Coach />} />
+        <Route path="/diet" element={<Diet />} />
+        <Route path="/fitness" element={<Fitness />} />
+        <Route path="/leaderboard" element={<Leaderboard />} />
+        <Route path="/calendar" element={<Calendar />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/settings" element={<Settings />} />
+      </Routes>
     </Router>
   );
 }
---
+++ client/src/components/Index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import "../css/main.css";
+
+const Index = () => {
+  return (
+    <div className="container homeContainer">
+      <h1>Welcome Page</h1>
+      <h1>Welcome to Healthify. Sign up or log in below.</h1>
+    </div>
+  );
+};
+
+export default Index;
