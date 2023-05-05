0fb974bfcc040a1950551af82a57d7356c830338
Gin Lu <glu23@my.bcit.ca>
2023-05-05 13:58:47 -0700

    Added in react-spring image fade to Index.jsx

--- client/src/components/Index.jsx
+++ client/src/components/Index.jsx
@@ -1,27 +1,45 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { useSpring, animated } from "react-spring";
 
-import "../css/index.css";
+import styles from "../css/index.module.css";
+import image from "../img/fitness.jpeg";
 
 const Index = () => {
+  const fadeIn = useSpring({
+    opacity: 1,
+    from: { opacity: 0 },
+    config: { duration: 1000 }
+  });
+
   return (
-    <div className="index-body">
+    <div className={styles["indexBody"]}>
       <div className="d-flex justify-content-center align-items-center h-100">
-        <div className="card-body index-card">
+        <div className={`card-body ${styles["indexCard"]}`}>
           <div className="d-flex flex-column align-items-center text-center">
-            <h1>Welcome to Healthify!</h1>
-            <h4>
+            <h1 className={styles["indexTitle"]}>Welcome to Healthify!</h1>
+            <animated.img
+              style={fadeIn}
+              src={image}
+              alt="Fitness"
+              className={styles["indexImage"]}
+            />
+            <h4 className={styles["indexDescription"]}>
               Healthify is a comprehensive fitness and dietary app designed to
               help users achieve their health goals. The app provides
               personalized fitness plans, workout routines, and meal plans based
               on users' unique needs, preferences, and goals.
             </h4>
-            <div className="button-group">
+            <div className={styles["button-group"]}>
               <Link to="/signup">
-                <button className="btn btn-primary btn-lg">Sign Up</button>
+                <button className={`btn btn-primary btn-lg ${styles["indexBtn"]}`}>
+                  Sign Up
+                </button>
               </Link>
               <Link to="/login">
-                <button className="btn btn-primary btn-lg">Log In</button>
+                <button className={`btn btn-primary btn-lg ${styles["indexBtn"]}`}>
+                  Log In
+                </button>
               </Link>
             </div>
           </div>
--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
-import './css/index.css';
+import './css/index.module.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
