40e03781d9b89516df4bfb15049530cf1bb0f47a
NikoDurano <nikodurano@gmail.com>
2023-05-06 16:37:51 -0700

    some more description comments in server.js

--- server/server.js
+++ server/server.js
@@ -17,21 +17,20 @@ const db = require("./database.js");
 const userRouter = require("./routes/users");
 const authRouter = require("./routes/auth");
 
-const { User } = require('./models/users');
-
-
+//THE USER MODEL
+const { User } = require("./models/users");
 
 const cors = require("cors");
 require("dotenv").config();
 
 const port = 8000;
 
+//OPENAI CONFIGURATION
 const configuration = new Configuration({
   organization: process.env.ORG,
   apiKey: process.env.AI,
 });
 const openai = new OpenAIApi(configuration);
-
 
 //THE CONNECTION TO DATABASE
 db();
@@ -44,57 +43,63 @@ app.use(express.json());
 app.use("/api/users", userRouter);
 app.use("/api/auth", authRouter);
 
-
-//GETS THE USER FROM THE EMAIL NEW CODE
-app.put('/users/:email',  async (req, res) => {
+//STORES USERS CHAT HISTORY TO THE DATABASE
+app.put("/users/:email", async (req, res) => {
+  //THE USERS EMAIL
   const userEmail = req.params.email;
+  //USERS CHAT HITORY
   const updatedUserData = req.body;
-
 
   try {
     const user = await User.findOneAndUpdate(
+      //FIND BY EMAIL
       { email: userEmail },
+      //SET THE MESSAGES TO THE UPDATED MESSAGES
       { $set: { messages: updatedUserData.messages } },
+      //NEW: RETURNS THE MODIFIED DOCUMENT RATHER THAN THE ORIGINAL.
+      //UPSERT: CREATES THE OBJECT IF IT DOESN'T EXIST OR UPDATES IT IF IT DOES.
       { new: true, upsert: true }
     );
-    
+
     res.status(200).json({
       message: `User with email ${userEmail} updated successfully`,
       user,
     });
   } catch (err) {
     console.error(err);
-    res.status(500).json({ error: 'Internal server error' });
+    res.status(500).json({ error: "Internal server error" });
   }
 });
-//END OF NEW CODE
 
-app.get('/coach/:email', async(req, res) => {
-
+//GETS THE USERS CHAT HISTORY FROM THE DATABASE
+app.get("/coach/:email", async (req, res) => {
+  //USERS EMAIL
   const userEmail = req.params.email;
 
-
-  try{
-  const user = await User.findOne({ email: userEmail });
-  if(!user){
-    return res.status(400).send('Email not registered' + userEmail);
-
-  }
-  const messages = user.messages;
-  const chatHistory = messages;
-  res.json(chatHistory);
-  }catch(e){
+  try {
+    //FINDS THE USER BY EMAIL
+    const user = await User.findOne({ email: userEmail });
+    if (!user) {
+      return res.status(400).send("Email not registered" + userEmail);
+    }
+    //GETS THE USERS CHAT HISTORY
+    const messages = user.messages;
+    //STORES THE USERS CHAT HISTORY
+    const chatHistory = messages;
+    //SENDS THE USERS CHAT HISTORY
+    res.json(chatHistory);
+  } catch (e) {
     console.log(e);
   }
-
 });
+
 //to generate and store a user's workout plan
-app.put('/fitness/:email',  async (req, res) => {
+app.put("/fitness/:email", async (req, res) => {
   const userEmail = req.params.email;
   // const newWorkout = req.body;
 
   // call and execute workouts.js
-  const workouts = require('./workouts');
+  const workouts = require("./workouts");
 
   function generateWorkout(callback) {
     workouts.generate((newWorkout) => {
@@ -103,18 +108,21 @@ app.put('/fitness/:email',  async (req, res) => {
   }
 
   async function updateWorkouts(newWorkout, callback) {
-    
     // console.log("newWorkout: " + newWorkout);
     // console.log("typeof newWorkout: " + typeof newWorkout);
     // console.log("typeof JSON.parse(newWorkout): " + typeof JSON.parse(newWorkout));
-    // console.log("newWorkout stringified: " + JSON.stringify(newWorkout));  
+    // console.log("newWorkout stringified: " + JSON.stringify(newWorkout));
     // console.log("callback: " + callback);
 
     try {
       const user = await User.findOneAndUpdate(
         { email: userEmail },
         // { $push: { workouts: newWorkout } }
-        { $push: { workouts: { $each: [JSON.parse(newWorkout)], $position: 0 } } } 
+        {
+          $push: {
+            workouts: { $each: [JSON.parse(newWorkout)], $position: 0 },
+          },
+        }
       );
       res.status(200).json({
         message: `New workout added to ${userEmail}.`,
@@ -122,7 +130,7 @@ app.put('/fitness/:email',  async (req, res) => {
       });
     } catch (err) {
       console.error(err);
-      res.status(500).json({ error: 'Internal server error' });
+      res.status(500).json({ error: "Internal server error" });
     }
     if (callback) {
       callback();
@@ -131,17 +139,17 @@ app.put('/fitness/:email',  async (req, res) => {
 
   // call the updateWorkouts function
   generateWorkout();
-  
 });
 
-
-app.post('/', async (req, res) => {
+app.post("/", async (req, res) => {
   const { message } = req.body;
   console.log(message);
 
   const response = await openai.createCompletion({
-      model: "text-davinci-003",
-      prompt: `${message}` + `. Return response in the following parsable JSON format:
+    model: "text-davinci-003",
+    prompt:
+      `${message}` +
+      `. Return response in the following parsable JSON format:
       [
       {
           "Day": "day",
@@ -154,48 +162,64 @@ app.post('/', async (req, res) => {
   ]
 
       `,
-      max_tokens: 1000,
-      temperature: 0,
-    });
+    max_tokens: 1000,
+    temperature: 0,
+  });
 
-    const parsableJson = response.data.choices[0].text;
+  const parsableJson = response.data.choices[0].text;
 
-    console.log(parsableJson);
+  console.log(parsableJson);
 
-  //   const stringJson = JSON.stringify(parsableJson);
+  const parsedJson = JSON.parse(parsableJson);
+  let messageOutTest = "";
 
-    const parsedJson = JSON.parse(parsableJson);
-    let messageOutTest = "";
+  parsedJson.forEach((choice) => {
+    if (choice.planType === "fitness") {
+      messageOutTest +=
+        "Day: " +
+        choice.Day +
+        "\n" +
+        "Exercise 1: " +
+        choice.item1 +
+        "\n" +
+        "Exercise 2: " +
+        choice.item2 +
+        "\n" +
+        "Exercise 3: " +
+        choice.item3 +
+        "\n" +
+        "Rest: " +
+        choice.item4 +
+        "\n" +
+        "\n";
+    } else {
+      messageOutTest +=
+        "Day: " +
+        choice.Day +
+        "\n" +
+        "BreakFast: " +
+        choice.item1 +
+        "\n" +
+        "Lunch: " +
+        choice.item2 +
+        "\n" +
+        "Dinner: " +
+        choice.item3 +
+        "\n" +
+        "Snack: " +
+        choice.item4 +
+        "\n" +
+        "\n";
+    }
+  });
 
-      parsedJson.forEach((choice) => {
-        if(choice.planType === "fitness"){
-          messageOutTest += "Day: " + choice.Day + "\n" +
-          "Exercise 1: " + choice.item1 + "\n" +
-          "Exercise 2: " + choice.item2 + "\n" +
-          "Exercise 3: " + choice.item3 + "\n" +
-          "Rest: " + choice.item4 + "\n" + "\n";
+  console.log(messageOutTest);
 
-        }else{
-          messageOutTest += "Day: " + choice.Day + "\n" +
-          "BreakFast: " + choice.item1 + "\n" +
-          "Lunch: " + choice.item2 + "\n" +
-          "Dinner: " + choice.item3 + "\n" +
-          "Snack: " + choice.item4 + "\n" +"\n";
-        }
-      });
- 
-    console.log(messageOutTest);
-
-
-    res.json(
-      {
-      message: messageOutTest
-      }
-    );
-
+  res.json({
+    message: messageOutTest,
+  });
 });
- 
+
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
 });
-
