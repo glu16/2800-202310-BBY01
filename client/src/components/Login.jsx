2525e2d1982da873596873833a715be32dbd4b80
Gin Lu <glu23@my.bcit.ca>
2023-05-05 23:07:10 -0700

    Login files now follow module.css convention

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,34 +1,25 @@
 import React from "react";
-import "../css/login.css";
-
 import { useState } from "react";
 import axios from "axios";
 
+import styles from "../css/login.module.css";
+
 function Login({ setToken }) {
-  //THE CODE FOR HOOKING UP THE BACKEND WITH THE FRONT WHEN WAS PRIMARLY FROM THIS VIDEO
-  //https://www.youtube.com/watch?v=HGgyd1bYWsE
   const [data, setData] = useState({
     email: "",
     password: "",
   });
   const [error, setError] = useState("");
 
-const handleSubmit = async(event) => {
-  event.preventDefault();
-  try{
-    const url = "http://localhost:8000/api/auth";
-    const {data:res} = await axios.post(url, data);
-    // localStorage.setItem("token", res.data);
-    setToken(res.data);
-    // email added to localStorage to pull for accessing database
-    localStorage.setItem("email", data.email);
-
-    window.location = "/";
-
-  }catch(error){
-    //ERROR IS CAUGHT HERE
-    console.log(error);
-
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const url = "http://localhost:8000/api/auth";
+      const { data: res } = await axios.post(url, data);
+      setToken(res.data);
+      localStorage.setItem("email", data.email);
+      window.location = "/";
+    } catch (error) {
       setError(error.response.data.message);
     }
   };
@@ -38,17 +29,19 @@ const handleSubmit = async(event) => {
   };
 
   return (
-    <div className="d-flex justify-content-center align-items-center h-100">
-      <div className="card">
+    <div
+      className={`d-flex justify-content-center align-items-center h-100 ${styles.loginContainer}`}
+    >
+      <div className={`card ${styles.loginCard}`}>
         <div className="card-body">
           <div className="d-flex flex-column align-items-center text-center">
             <h1>Login</h1>
-            <form id="login" onSubmit={handleSubmit}>
+            <form className={styles.login} onSubmit={handleSubmit}>
               <label htmlFor="email-input"></label>
               <input
                 type="email"
                 id="email-input"
-                className="user-input"
+                className={`user-input ${styles.userInput}`}
                 placeholder="Email"
                 name="email"
                 value={data.email}
@@ -61,18 +54,22 @@ const handleSubmit = async(event) => {
               <input
                 type="password"
                 id="password-input"
-                className="user-input"
+                className={`user-input ${styles.userInput}`}
                 name="password"
                 value={data.password}
                 onChange={handleChange}
                 placeholder="Password"
                 required
               />
-              {/* ERROR IS DISPLAYED */}
               {error && <div>{error}</div>}
 
               <label htmlFor="submit-btn"></label>
-              <input type="submit" id="login-btn" value="Login" />
+              <input
+                type="submit"
+                id="login-btn"
+                value="Login"
+                className={`btn btn-primary ${styles.loginBtn}`}
+              />
             </form>
           </div>
         </div>
