da627608d47f76ba3a6a7b7b2abf84ff0706274a
Niko Durano <nikodurano@gmail.com>
2023-05-08 09:01:54 -0700

    documation in server

--- server/server.js
+++ server/server.js
@@ -26,7 +26,7 @@ require("dotenv").config();
 
 const port = 8000;
 
-//OPENAI CONFIGURATION
+// OPENAI CONFIGURATION
 const configuration = new Configuration({
   organization: process.env.ORG,
   apiKey: process.env.AI,
@@ -59,7 +59,7 @@ app.get("/getFromUser/:email", async (req, res) => {
     if (!user) {
       return res.status(400).send("Email not registered" + userEmail);
     }
-    //REPLACE 'REPLACE_ME' WITH KEY OF DATA YOU WANT TO GET
+    // REPLACE 'REPLACE_ME' WITH KEY OF DATA YOU WANT TO GET
     const item = user.REPLACE_ME;
     res.json(item);
   } catch (e) {
