11fd16624ce92566e78e1bc35b6be837ed8c44bb
Felix Wei <felixwei7@gmail.com>
2023-05-09 15:30:21 -0700

    temporary save while switching to dev

--- client/src/components/Fitness.jsx
+++ client/src/components/Fitness.jsx
@@ -66,21 +66,29 @@ async function getWorkout() {
     headers: { "Content-Type": "application/json", },
   });
   var data = await response.json(); 
-  workout = data[0];
 
-  // console.log(data[0]); //full week
-  // console.log(data[0].Day1); //day
-  // console.log(data[0].Day1.Exercise2); //exericse
-  // console.log(data[0].Day1.Exercise2.calories); //exercise-details
-  console.log(data[0])
-
-  return data[0];
+  // check if workouts is empty
+  if (data == "empty") {
+    return "empty";
+  } else {
+    workout = data[0];
+    // console.log(data[0]); //full week
+    // console.log(data[0].Day1); //day
+    // console.log(data[0].Day1.Exercise2); //exericse
+    // console.log(data[0].Day1.Exercise2.calories); //exercise-details
+    console.log(data[0])
+    return data[0];
+  }
 }
 
 
 // display user's workout, can't be async
 function Workout() {
   const [workout, setWorkout] = useState(null);
+
+  // if (getWorkout() == "empty") {
+  //   useEffect(()
+  // }
 
   useEffect(() => {
     async function fetchData() {
