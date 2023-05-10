b61583ab69093f529e1b30e2eadb93b01e88446d
Leroy Lau <llau65@my.bcit.ca>
2023-05-10 16:08:14 -0700

    Updated API call to retrieve data using username

--- client/src/components/Profile.jsx
+++ client/src/components/Profile.jsx
@@ -13,7 +13,7 @@ const Profile = () => {
     async function fetchUserData() {
       try {
         const response = await axios.get(
-          `https://healthify-enxj.onrender.com/users/${localStorage.getItem("email")}`,
+          `https://healthify-enxj.onrender.com/users/${localStorage.getItem("username")}`,
           {
             headers: {
               Authorization: `Bearer ${localStorage.getItem("token")}`,
