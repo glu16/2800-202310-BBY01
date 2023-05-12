39cdedf7c8ce688f42e1cf5592843b44393bda34
Gin Lu <glu23@my.bcit.ca>
2023-05-12 16:48:53 -0700

    Created useState hook for global and local ranks

--- client/src/components/Leaderboard.jsx
+++ client/src/components/Leaderboard.jsx
@@ -4,6 +4,7 @@ import axios from "axios";
 import styles from "../css/leaderboard.module.css";
 
 const Leaderboard = () => {
+  // Retrieves the users from the database
   const [users, setUsers] = useState([]);
 
   useEffect(() => {
@@ -17,6 +18,61 @@ const Leaderboard = () => {
     };
     fetchUsers();
   }, []);
+  // End of user retrieval
+
+  // Allows users to switch between global and local leaderboards
+  const [activeTab, setActiveTab] = useState("global");
+
+  const handleTabChange = (tab) => {
+    setActiveTab(tab);
+  };
+
+  const renderLeaderboard = () => {
+    if (activeTab === "global") {
+      return (
+        <table>
+          <thead>
+            <tr>
+              <th>Rank</th>
+              <th>Name</th>
+              <th>Points</th>
+            </tr>
+          </thead>
+          <tbody>
+            {users.map((user, index) => (
+              <tr key={user._id}>
+                <td>{index + 1}</td>
+                <td>{user.name}</td>
+                <td>{user.points}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      );
+    } else if (activeTab === "local") {
+      return (
+        <table>
+          <thead>
+            <tr>
+              <th>Rank</th>
+              <th>Name</th>
+              <th>Points</th>
+            </tr>
+          </thead>
+          <tbody>
+            {users.map((user, index) => (
+              <tr key={user._id}>
+                <td>{index + 1}</td>
+                <td>{user.name}</td>
+                <td>{user.points}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      );
+    }
+  };
+  // End of global to local leaderboard switch
 
   return (
     <div className={styles.cardWrapper}>
@@ -26,24 +82,21 @@ const Leaderboard = () => {
         <div className="card-body">
           <div className="d-flex flex-column align-items-center text-center">
             <h1 className={styles.ranksHeader}>Leaderboard Ranks</h1>
-            <table>
-              <thead>
-                <tr>
-                  <th>Rank</th>
-                  <th>Name</th>
-                  <th>Points</th>
-                </tr>
-              </thead>
-              <tbody>
-                {users.map((user, index) => (
-                  <tr key={user._id}>
-                    <td>{index + 1}</td>
-                    <td>{user.name}</td>
-                    <td>{user.points}</td>
-                  </tr>
-                ))}
-              </tbody>
-            </table>
+            <div className={styles.tabs}>
+              <button
+                className={activeTab === "global" ? styles.activeTab : ""}
+                onClick={() => handleTabChange("global")}
+              >
+                Global Leaderboard
+              </button>
+              <button
+                className={activeTab === "local" ? styles.activeTab : ""}
+                onClick={() => handleTabChange("local")}
+              >
+                Local Leaderboard
+              </button>
+            </div>
+            {renderLeaderboard()}
           </div>
         </div>
       </div>
