a85ba838649e5082663387fa27209e4c6c2f5a83
Gin Lu <glu23@my.bcit.ca>
2023-05-11 21:16:40 -0700

    Fixed colour for before login navbar

--- client/src/components/Contact.jsx
+++ client/src/components/Contact.jsx
@@ -1,25 +1,23 @@
-import React, {useState} from "react";
-import {useSpring, animated} from "react-spring";
+import React, { useState } from "react";
+import { useSpring, animated } from "react-spring";
 
-import "../css/main.css";
+import styles from "../css/about.module.css";
 
 const Contact = () => {
   const greetings = useSpring({
     opacity: 1,
-    from: {opacity: 0},
+    from: { opacity: 0 },
     delay: 500,
   });
 
   return (
-    <div className="d-flex justify-content-center align-items-center h-100">
-      <div className="card">
-        <div className="card-body">
-          <div className="d-flex flex-column align-items-center text-center">
-            <animated.h1 className="display-6" style={greetings}>
-              Contact Us
-            </animated.h1>
-          </div>
-        </div>
+    <div
+      className={`d-flex justify-content-center align-items-center h-100 ${styles.aboutBody}`}
+    >
+      <div className={`card-body ${styles.aboutCard}`}>
+        <animated.h1 className={styles.aboutHeader} style={greetings}>
+          Contact Us
+        </animated.h1>
       </div>
     </div>
   );
