002d6f05f001bac3a90236099b1c95f31bd2003d
Gin Lu <glu23@my.bcit.ca>
2023-05-11 23:10:57 -0700

    Playing around with React's draggable list

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { useSpring, animated } from "react-spring";
+import { useSpring, useSprings, animated } from "react-spring";
 import { ProgressBar } from "react-step-progress-bar";
 import axios from "axios";
 
@@ -7,13 +7,48 @@ import "react-step-progress-bar/styles.css";
 import styles from "../css/home.module.css";
 
 const Home = () => {
-  /* Text animation */
+  /* React Spring animation to create text animation */
   const greetings = useSpring({
     opacity: 1,
     from: { opacity: 0 },
     delay: 500,
   });
   /* End of text animation */
+
+  /* React Spring animation to create a draggable list */
+  const listData = [
+    { id: 1, text: "Deadlift: 4 Sets x 8 Reps" },
+    { id: 2, text: "Seated Cable Row: 3 Sets x 12 Reps" },
+    { id: 3, text: "Bench Press: 4 Sets x 8 Reps" },
+  ];
+
+  const [springs, setSprings] = useSprings(listData.length, (index) => ({
+    y: index * 70,
+    zIndex: listData.length - index,
+  }));
+
+  const handleDrag = (index) => (event) => {
+    setSprings((prev) => {
+      const prevArray = Array.isArray(prev) ? prev : [prev];
+      return prevArray.map((props, i) => {
+        return i === index
+          ? { ...props, y: event.clientY - 35, zIndex: listData.length }
+          : props;
+      });
+    });
+  };
+
+  const handleDragEnd = (index) => {
+    setSprings((prev) => {
+      const prevArray = Array.isArray(prev) ? prev : [prev];
+      return prevArray.map((props, i) => {
+        return i === index
+          ? { ...props, y: index * 70, zIndex: listData.length - index }
+          : props;
+      });
+    });
+  };
+  /* End of draggable list animation */
 
   const [userName, setUserName] = useState("");
 
@@ -22,9 +57,7 @@ const Home = () => {
     async function fetchUserName() {
       try {
         const response = await axios.get(
-          `http://localhost:5050/users/${localStorage.getItem(
-            "username"
-          )}`,
+          `http://localhost:5050/users/${localStorage.getItem("username")}`,
           {
             headers: {
               Authorization: `Bearer ${localStorage.getItem("token")}`,
@@ -142,23 +175,26 @@ const Home = () => {
         </div>
       </div>
       <div
-        className={`d-flex justify-content-center align-items-center h-100 ${styles.progressCard}`}
+        className={`d-flex justify-content-center align-items-center h-100 ${styles.listCard}`}
       >
-        <div className={styles.progressCard}>
-          <h4 className={styles.progressHeader}>Fitness Tracker</h4>
-          <div className="progress-bar-container">
-            <ProgressBar
-              percent={fitnessProgress}
-              filledBackground="linear-gradient(to right, #fefb72, #f0bb31)"
-            />
-          </div>
-          <button
-            className={`btn btn-primary ${styles.progressBtn}`}
-            onClick={() => handleFitnessProgressChange(25)}
+        {springs.map((props, index) => (
+          <animated.div
+            key={listData[index].id}
+            style={{
+              ...props,
+              position: "absolute",
+              width: "100%",
+              height: "70px",
+              cursor: "grab",
+            }}
+            onMouseDown={handleDrag(index)}
+            onTouchStart={handleDrag(index)}
+            onMouseUp={() => handleDragEnd(index)}
+            onTouchEnd={() => handleDragEnd(index)}
           >
-            Update Progress
-          </button>
-        </div>
+            {listData[index].text}
+          </animated.div>
+        ))}
       </div>
     </div>
   );
