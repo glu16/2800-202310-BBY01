8e18cc740e752a3c2086eed3c22c671690d4c0f0
NikoDurano <nikodurano@gmail.com>
2023-05-15 18:17:46 -0700

    diet works

--- server/diet.js
+++ server/diet.js
@@ -1,5 +1,10 @@
 require("dotenv").config();
 
+if (typeof localStorage === "undefined" || localStorage === null) {
+  var LocalStorage = require('node-localstorage').LocalStorage;
+  localStorage = new LocalStorage('./scratch');
+}
+
 //This page is bascially the same as workout.js, but it is for diet plans instead of workout plans.
 
 const { Configuration, OpenAIApi } = require("openai");
@@ -24,49 +29,56 @@ const stopProgress = () => {
   process.stdout.cursorTo(0);
 };
 
+const username = localStorage.getItem("username");
 
-/* 
-(SOME GET METHOD THAT GETS THE USERS PREFERENCES FOR THE DIET PLAN)
-var sex;
-var age;
-var height;
-var weight;
-var activityLevel;
-var goal;
+async function getUserStats() {
+  var response = await fetch(`http://localhost:5050/userStats/${username}`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+  var data = await response.json();
+  return data;
+}
 
-var foodRestrictions = [];
+function generate(callback) {
+getUserStats().then((data) => {
+  console.log(data);
+
+  var sex = data.sex;
+  var age = data.age;
+  var height = data.height;
+  var weight = data.weight;
+  var activityLevel = data.activityLevel;
+  var goal = data.goal;
+
+  var foodRestrictions = [];
 var foodPreferences = [];
 
-if foodRestrictions.length == 0 {
+if (foodRestrictions.length == 0) {
     foodRestrictions = ["none"];
 }
-if foodPreferences.length == 0 {
+if (foodPreferences.length == 0) {
     foodPreferences = ["none"];
 }
 
-var inputPromt = `I am a ${age} ${sex} and I am ${height} cm tall and weigh ${weight} kilograms.
-My activity level is ${activityLevel} and my goal is to ${goal}.
-My restrictions are ${restrictions.join(", ")} and my preferences are ${preferences.join(", ")}.`
+  let inputPrompt = `I am a ${age} ${sex} and I am ${height} m tall and weigh ${weight} kilograms. My activity level is ${activityLevel} and my goal is to ${goal}. My restrictions are ${foodRestrictions.join(", ")} and my preferences are ${foodPreferences.join(", ")}. `
 
+  inputPrompt += "Give me a 7-day diet plan. ";
+  inputPrompt += "Give me at least five meal options for each day. ";
+  inputPrompt += "Format each day with a number like Day 1 or Day 7. Do not use day names like Monday. ";
+  inputPrompt += "Format each meal option like this example sentance: Meal 1: Broccoli and chicken fajitas (Protein: 25g, Carbs: 20g, Fat: 8g) Calories: 270";
+  
+  console.log(inputPrompt);
 
-REST OF THE PROMPT ABOUT DIET....
-*/
-const foods = ["chicken", "broccoli"];
-const level = "intermediate";
-var inputPrompt =
-  "Give me a " +
-  level +
-  " level, 7-day diet plan with a focus on the following foods:" +
-  foods.join(", ") +
-  ". ";
-const environment = "home-cooked";
-inputPrompt += "I only want " + environment + " meals. ";
-inputPrompt += "Give me at least five meal options for each day. ";
-inputPrompt +=
-  "Format each day with a number like Day 1 or Day 7. Do not use day names like Monday. ";
-inputPrompt +=
-  "Format each meal option like this example sentance: Meal 1: Broccoli and chicken fajitas (Protein: 25g, Carbs: 20g, Fat: 8g) Calories: 270";
+  runAI(inputPrompt).then((result) => {
+    const newDietPlan = result;
+    callback(newDietPlan);
+  });
 
+});
+}
 const runAI = async (input) => {
   console.log("Generating diet plan...");
   startProgress();
@@ -186,12 +198,8 @@ const runAI = async (input) => {
   console.log("...diet plan generated.");
   return JSON.stringify(dietPlan);
 };
-function generate(callback) {
-  runAI(inputPrompt).then((result) => {
-    const newWorkout = result;
-    callback(newWorkout);
-  });
-}
+
+
 module.exports = {
   generate: generate,
 };
