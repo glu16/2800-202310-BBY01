00990e146247d631f6e3d73805451d43646b6b16
Gin Lu <glu23@my.bcit.ca>
2023-05-23 16:11:22 -0700

    Retrieved workout arrays and converted them to calendar events, need to display properly

--- client/src/components/Calendar.jsx
+++ client/src/components/Calendar.jsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from "react";
 import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
-import axios from "axios";
 
 import styles from "../css/calendar.module.css";
 
@@ -9,23 +8,70 @@ const Calendar = () => {
   const [events, setEvents] = useState([]);
 
   useEffect(() => {
-    axios
-      .get(`https://healthify-server.vercel.app/calendar/${localStorage.getItem("username")}`)
-      .then((response) => {
-        const plans = response.data;
+    async function fetchWorkout() {
+      try {
+        const response = await fetch(
+          `https://healthify-server.vercel.app/fitness/${localStorage.getItem(
+            "username"
+          )}`
+        );
+        const data = await response.json();
 
-        // Map function to display the plans on the calendar
-        const calendarEvents = plans.map((plan) => ({
-          title: plan.title,
-          date: plan.date,
-        }));
+        if (data === "empty") {
+          setEvents([]);
+        } else {
+          if (typeof data === "object") {
+            const workouts = Object.entries(data);
+            console.log("Workouts:", workouts);
 
-        setEvents(calendarEvents);
-      })
-      .catch((error) => {
-        console.error("Error retrieving plans:", error);
-      });
+            const calendarEvents = workouts
+              .map(([date, exercises]) => {
+                const formattedDate = new Date(date).toLocaleDateString(
+                  undefined,
+                  {
+                    weekday: "long",
+                    year: "numeric",
+                    month: "long",
+                    day: "numeric",
+                  }
+                );
+
+                const exercisesList = Object.values(exercises);
+                console.log("Exercises:", exercisesList);
+
+                return exercisesList.map((exercise) => ({
+                  title: exercise.name,
+                  date: formattedDate,
+                  extendedProps: {
+                    setsAndReps: exercise.setsAndReps,
+                    calories: exercise.calories,
+                  },
+                }));
+              })
+              .flat();
+
+            console.log("Calendar events:", calendarEvents);
+            setEvents([...calendarEvents]);
+          } else {
+            console.log("Invalid data format:", data);
+          }
+        }
+      } catch (error) {
+        console.error("Error retrieving workout:", error);
+      }
+    }
+
+    fetchWorkout();
   }, []);
+
+  function renderEventContent(eventInfo) {
+    return (
+      <div>
+        <h3>{eventInfo.event.title}</h3>
+        <p>{eventInfo.event.extendedProps.setsAndReps}</p>
+      </div>
+    );
+  }
 
   return (
     <div className={styles.calendarBody}>
@@ -43,6 +89,7 @@ const Calendar = () => {
                   plugins={[dayGridPlugin]}
                   initialView="dayGridMonth"
                   events={events}
+                  eventContent={renderEventContent}
                 />
               </div>
             </div>
