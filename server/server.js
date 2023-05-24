2792c7c010923a03bc7bb652e49a4da22f6fd92f
NikoDurano <nikodurano@gmail.com>
2023-05-23 18:49:31 -0700

    test

--- server/server.js
+++ server/server.js
@@ -37,12 +37,7 @@ db();
 app.use(cors());
 app.use(express.json());
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  next();
-});
+
 // ROUTES
 app.use("/api/users", userRouter);
 app.use("/api/auth", authRouter);
