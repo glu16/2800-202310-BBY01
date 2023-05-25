6f21d6dac5710a8340253a32dd084f8e159fe3d8
Leroy Lau <llau65@my.bcit.ca>
2023-05-24 21:38:46 -0700

    Added in code to limit incrementing of progress tracker

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -215,9 +215,7 @@ const Home = () => {
         console.error("Error fetching streak:", error);
       }
     }
-    useEffect(() => {
-      getStreak();
-    }, []); // Empty dependency array ensures the effect runs only once, similar to componentDidMount
+    getStreak();
 
     // Render loading state if streak data is not yet available
     if (currentStreak === null || longestStreak === null) {
@@ -502,7 +500,11 @@ const Home = () => {
 
   // Click event handler to increment the diet progress
   const handleDietProgressChange = () => {
+    if (dietProgress < 100){
     setDietProgress(dietProgress + 25);
+  } else {
+    return;
+  }
   };
 
   // useState hook variables for the fitness progress
